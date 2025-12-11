768ca6f428e5b85efd52581799eac912e84da7e7
rgs1119timo12 <75741785+rgs1119timo12@users.noreply.github.com>
2025-12-11 16:47:23 +0900

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
-
-
+import * as iam from 'aws-cdk-lib/aws-iam';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
@@ -17,4 +16,16 @@ backend.addOutput({
   },
 });
 
+
+const s3BucketPolicy = new iam.PolicyStatement({
+  effect: iam.Effect.ALLOW,
+  actions: ['s3:ListBucket', 's3:GetObject', 's3:PutObject', 's3:DeleteObject'], // 필요한 권한 나열
+  resources: [
+    'arn:aws:s3:::unban-styleguide',      // 버킷 자체
+    'arn:aws:s3:::unban-styleguide/*'     // 버킷 내의 모든 객체
+  ],
+});
+
+// 로그인한 사용자(Authenticated)에게 권한 부여
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(s3Policy);
 

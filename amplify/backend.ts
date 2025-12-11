acb4826aaffce09110a9581e711998fb2a038b95
rgs1119timo12 <75741785+rgs1119timo12@users.noreply.github.com>
2025-12-11 16:43:44 +0900

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,8 +7,14 @@ import { storage } from './storage/resource';
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  auth,
-  storage
+  auth
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: 'ap-northeast-2', // 버킷이 있는 리전을 정확히 입력하세요 (예: 서울)
+    bucket_name: 'unban-styleguide', // 연결할 기존 버킷 이름
+  },
 });
 
 

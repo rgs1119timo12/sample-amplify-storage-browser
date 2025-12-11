6ab395537f79a49414ae8723e5a10b89f7162715
rgs1119timo12 <75741785+rgs1119timo12@users.noreply.github.com>
2025-12-11 16:36:29 +0900

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,6 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'myStorageBucket',
+  bucketName: 'unban-styleguide',
   isDefault: true,
    access: (allow) => ({
     'public/*': [
